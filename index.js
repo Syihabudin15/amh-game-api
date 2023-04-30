bfbbeb43257fa9051239eccc1b7f99c202569c4f
Syihabudin Tsani <syihabudintsani15@gmail.com>
2023-04-30 18:24:59 +0700

    [Improve] change envirotment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = Express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(Express.json());
--- src/Configs/DbConfig.js
+++ src/Configs/DbConfig.js
@@ -2,7 +2,7 @@ import { Sequelize, DataTypes, Op } from "sequelize";
 import {} from "dotenv/config";
 
 const dbHost = process.env.DB_HOST;
-const dbName = process.env.DB_DATABASE;
+const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
 
