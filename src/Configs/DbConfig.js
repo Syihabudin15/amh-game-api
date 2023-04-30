9e21b172298028048ff326690a0f3f7df61b190e
Syihabudin Tsani <syihabudintsani15@gmail.com>
2023-04-30 18:31:24 +0700

    [improve] change env

--- src/Configs/DbConfig.js
+++ src/Configs/DbConfig.js
@@ -5,9 +5,11 @@ const dbHost = process.env.DB_HOST;
 const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
+const dbPort = process.env.DB_PORT;
 
 const DB = new Sequelize(dbName, dbUser, dbPass, {
     host: dbHost,
+    port: dbPort,
     dialect: 'mysql'
 });
 
